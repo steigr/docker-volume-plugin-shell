2cc2f9aa358e64495fb898a985145ab132627791
Mathias Kaufmann <me@stei.gr>
2016-12-07 15:00:24 +0100

    Make mounts shared
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- driver/docker-entrypoint.sh
+++ driver/docker-entrypoint.sh
@@ -5,4 +5,6 @@ set -eo pipefail
 
 cd /driver
 mkdir -p /var/run/docker/plugins/$DRIVER_NAME || true
+[[ -d $MOUNTS ]] || mkdir -p $MOUNTS
+mount --make-rshared $MOUNTS
 exec bundler exec puma --config=puma.rb
